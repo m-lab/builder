291747546dfba84d25ea248b60479f1bbcc0d9e8
Greg Russell <gfr10598@gmail.com>
2017-03-22 10:55:03 -0400

    Update to use m-lab/ndt instead of -3.7

--- scripts/ndt_build_and_test.sh
+++ scripts/ndt_build_and_test.sh
@@ -9,13 +9,13 @@ DISABLE_APPLET_SIGNING=1 ~/builder/build.sh iupui_ndt
 
 # Run basic unit tests that don't require web100.
 LD_LIBRARY_PATH=/home/iupui_ndt/build/lib NDT_HOSTNAME=localhost \
-./iupui_ndt/ndt-3.7.0.1/src/web100_testoptions_unit_tests
+./iupui_ndt/ndt/src/web100_testoptions_unit_tests
 
 LD_LIBRARY_PATH=/home/iupui_ndt/build/lib NDT_HOSTNAME=localhost \
-./iupui_ndt/ndt-3.7.0.1/src/web100_websocket_unit_tests
+./iupui_ndt/ndt/src/web100_websocket_unit_tests
 
 # This test doesn't currently work on travis because it requires
 # web100 patched kernel.  When we no longer have disabled tests, we
 # should just use "make check" to run all the tests.
 # LD_LIBRARY_PATH=/home/iupui_ndt/build/lib NDT_HOSTNAME=localhost \
-# ~/builder/iupui_ndt/ndt-3.7.0.1/src/web100srv_unit_tests
+# ~/builder/iupui_ndt/ndt/src/web100srv_unit_tests
