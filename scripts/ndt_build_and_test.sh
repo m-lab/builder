456d4bb12dc2c80ea4adb2223a09c6d21b37c3f1
Greg Russell <gfr10598@gmail.com>
2017-03-23 17:24:23 -0400

    update travis to build ndt-support dev branch

--- scripts/ndt_build_and_test.sh
+++ scripts/ndt_build_and_test.sh
@@ -2,10 +2,16 @@
 set -x
 set -e
 
+BRANCH_OR_TAG=${1:-master}
+
 # Use a clean directory, so that we can mount it from docker run and
 # have access to the finished product, when required.
-mkdir ~/ndt; cd ~/ndt; cp ~/builder/slice-tags.list .
-DISABLE_APPLET_SIGNING=1 ~/builder/build.sh iupui_ndt
+mkdir ~/ndt; cd ~/ndt
+
+# This builds the branch or tag specified as $1.  If no argument, then
+# it builds the master branch.
+DISABLE_APPLET_SIGNING=1 ~/builder/build_one.sh \
+  https://github.com/m-lab/ndt-support.git ${BRANCH_OR_TAG} iupui_ndt
 
 # Run basic unit tests that don't require web100.
 LD_LIBRARY_PATH=/home/iupui_ndt/build/lib NDT_HOSTNAME=localhost \
